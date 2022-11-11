8133c02bd21391c464e925b0efecfdd236305953
Pawee Tantivasdakarn <mewpawee@Pawees-Mac-mini.local>
2022-11-12 00:55:40 +0700

    add all

--- rust/cheat-sheet/src/main.rs
+++ rust/cheat-sheet/src/main.rs
@@ -26,6 +26,31 @@ fn create_2d(_x: u32, _y: u32){
     let point = Point{x: _x, y: _y};
     println!("My X, {}\nMy Y, {}", point.x, point.y);
 } 
+
+fn iterate_list() {
+    let items: [i32; 4] = [1, 2, 3, 4];
+    // first implemetation
+    println!("fist run");
+    for item in items {
+        println!("item: {}", item);
+    }
+    // another implementation
+    println!("second run");
+    items.into_iter().for_each(|item| println!("item: {}", item));
+
+    //third run
+    println!("third run");
+    for (i, x) in items.iter().enumerate(){
+        println!("item: {} = {}", i, x);
+    }
+
+    //fourth run
+    println!("fourth run");
+    items.iter().enumerate().for_each(|(i, x)| {
+        println!("item: {} = {}", i, x);
+    })
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args[1].as_str(){
@@ -38,6 +63,7 @@ fn main() {
             println!("{}",result);
         },
         "5" => create_2d(1,2),
+        "6" => iterate_list(),
         _ => println!("No implementation"),
     };
 }
